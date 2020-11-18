5bfaa0d3cb7b5f415f81ab982f11528a9032cfdb
RyouMisakl <33336762+RyouMisakl@users.noreply.github.com>
2020-11-19 02:18:30 +0800

    greybox stage, along with teacher able to throw chalk at random time (able to throw at random student too but the random number is not setup in script yet) and chalk is able to movetowards the target but the rotation is not yet right (still haven add the destory on impact yet)

---
+++ Flying Chalk/Assets/Scripts/Chalk.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chalk : MonoBehaviour
+{
+    [SerializeField] private float smooth = 5.0f;
+    [SerializeField] private float movespeed = -1f;
+    private Vector3 shootDir;
+    private Quaternion target;
+     
+    public void setup(Vector3 shootDir)
+    {
+        this.shootDir = shootDir;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Rotate the cube by converting the angles into a quaternion.
+        target = Quaternion.Euler(-90, this.transform.rotation.y, this.transform.rotation.z);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+        transform.position += shootDir * movespeed * Time.deltaTime;
+    }
+}
---
+++ Flying Chalk/Assets/Scripts/Teacher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Teacher : MonoBehaviour
+{
+    [SerializeField] private Transform chalk;
+    [SerializeField] private Transform firepoint;
+    [SerializeField] private float min, max;
+    [SerializeField] private Transform[] targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnChalk", 1f);
+    }
+
+    void SpawnChalk()
+    {
+        //shoot chalk at random students or player
+        Transform throwchalk = Instantiate(chalk, firepoint.position,Quaternion.identity);
+        Vector3 shootDir = (firepoint.position - targets[0].position).normalized;
+        throwchalk.GetComponent<Chalk>().setup(shootDir);
+
+        //random time to call this function again
+        float ramTime = Random.Range(min,max);
+        Invoke("SpawnChalk", ramTime);
+    }
+ 
+}
