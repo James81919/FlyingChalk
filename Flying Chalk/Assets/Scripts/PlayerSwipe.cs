19603e00a0f3d4bbfe086d43e316c5c4aaf0622b
James Johnstone <jjohnstone2520@gmail.com>
2020-11-19 18:08:36 +1300

    ~Added swipe detection ~Added swipe detection script ~Added player swipe script (which will hold logic to redirecting chalk)

---
+++ Flying Chalk/Assets/Scripts/PlayerSwipe.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwipe : MonoBehaviour
+{
+    private void Awake()
+    {
+        SwipeDetector.OnSwipe += RedirectChalk;
+    }
+
+    private void RedirectChalk(SwipeData data)
+    {
+        Vector2 swipeDirection;
+        swipeDirection = (data.EndPosition - data.StartPosition).normalized;
+        Debug.Log("Swipe Direction = " + swipeDirection);
+    }
+}
---
+++ Flying Chalk/Assets/Scripts/SwipeDetector.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using UnityEngine;
+
+public struct SwipeData
+{
+    public Vector2 StartPosition;
+    public Vector2 EndPosition;
+    public SwipeDirection Direction;
+}
+
+public enum SwipeDirection
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+public class SwipeDetector : MonoBehaviour
+{
+    private Vector2 fingerDownPosition;
+    private Vector2 fingerUpPosition;
+
+    [SerializeField]
+    private bool detectSwipeOnlyAfterRelease = false;
+
+    [SerializeField]
+    private float minDistanceForSwipe = 20f;
+
+    public static event Action<SwipeData> OnSwipe = delegate { };
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            fingerUpPosition = Input.mousePosition;
+            fingerDownPosition = Input.mousePosition;
+        }
+
+        if (!detectSwipeOnlyAfterRelease && Input.GetMouseButton(0))
+        {
+            fingerDownPosition = Input.mousePosition;
+            DetectSwipe();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            fingerDownPosition = Input.mousePosition;
+            DetectSwipe();
+        }
+#else
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                fingerUpPosition = touch.position;
+                fingerDownPosition = touch.position;
+            }
+
+            if (!detectSwipeOnlyAfterRelease && touch.phase == TouchPhase.Moved)
+            {
+                fingerDownPosition = touch.position;
+                DetectSwipe();
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                fingerDownPosition = touch.position;
+                DetectSwipe();
+            }
+        }
+#endif
+    }
+
+    private void DetectSwipe()
+    {
+        if (SwipeDistanceCheckMet())
+        {
+            if (IsVerticalSwipe())
+            {
+                var direction = fingerDownPosition.y - fingerUpPosition.y > 0 ? SwipeDirection.UP : SwipeDirection.DOWN;
+                SendSwipe(direction);
+            }
+            else
+            {
+                var direction = fingerDownPosition.x - fingerUpPosition.x > 0 ? SwipeDirection.RIGHT : SwipeDirection.LEFT;
+                SendSwipe(direction);
+            }
+            fingerUpPosition = fingerDownPosition;
+        }
+    }
+
+    private bool IsVerticalSwipe()
+    {
+        return VerticalMovementDistance() > HorizontalMovementDistance();
+    }
+
+    private bool SwipeDistanceCheckMet()
+    {
+        return VerticalMovementDistance() > minDistanceForSwipe || HorizontalMovementDistance() > minDistanceForSwipe;
+    }
+
+    private float VerticalMovementDistance()
+    {
+        return Mathf.Abs(fingerDownPosition.y - fingerUpPosition.y);
+    }
+
+    private float HorizontalMovementDistance()
+    {
+        return Mathf.Abs(fingerDownPosition.x - fingerUpPosition.x);
+    }
+
+    private void SendSwipe(SwipeDirection direction)
+    {
+        SwipeData swipeData = new SwipeData()
+        {
+            Direction = direction,
+            StartPosition = fingerDownPosition,
+            EndPosition = fingerUpPosition
+        };
+        OnSwipe(swipeData);
+    }
+}
